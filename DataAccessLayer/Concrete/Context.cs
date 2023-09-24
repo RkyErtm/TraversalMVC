b630b538b5b79229ab4231f460a069c80fc37344
RkyErtm <rkyertm1209@gmail.com>
2023-09-24 14:59:59 +0300

    updated

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -8,23 +8,24 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete
 {
-    public class Context :DbContext
+    public class Context : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=.\\MSSQLSERVER01;database=TraversalDB;integrated security=true;");
+            optionsBuilder.UseSqlServer("Server=localhost;database=TraversalDB;integrated security=true;");
             //base.OnConfiguring(optionsBuilder);
+        }
 
-            public DbSet<About> Abouts { get; set; }
-            public DbSet<About2> About2s { get; set; }
-            public DbSet<Contact> Contacts { get; set; }
-            public DbSet<Destination> Destinations { get; set; }
-            public DbSet<Feature> Features { get; set; }
-            public DbSet<Feature2> Feature2s { get; set; }
-            public DbSet<Guide> Guides { get; set; }
-            public DbSet<Newslatter> Newslatters { get; set; }
-            public DbSet<SubAbout>  SubAbouts{ get; set; }
-            public DbSet<Testimonial> Testimonials{ get; set; }
-    }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<About2> About2s { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Feature2> Feature2s { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<Newslatter> Newslatters { get; set; }
+        public DbSet<SubAbout> SubAbouts { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
     }
 }
+
