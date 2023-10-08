38a34bc386098f92df52af2a303e3394b73c0e11
RkyErtm <rkyertm1209@gmail.com>
2023-10-08 22:54:09 +0300

    lesson 16 finished

---
+++ Traversal/ViewComponents/Default/_PopularDestinations.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.ViewComponents.Default
+{
+    public class _PopularDestinations : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+            var values=destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
