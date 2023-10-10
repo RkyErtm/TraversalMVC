cea1660e398607eee9e5cebbd888560a7cb0cc05
RkyErtm <rkyertm1209@gmail.com>
2023-10-10 23:04:05 +0300

    lesson 18

---
+++ BusinessLayer/Concrete/TestimonalManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class TestimonalManager : ITestimonialService
+    {
+        ITestimonialDal _testimonalDal;
+
+        public TestimonalManager(ITestimonialDal testimonalDal)
+        {
+            _testimonalDal = testimonalDal;
+        }
+
+        public void TAdd(Testimonial t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Testimonial t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Testimonial TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Testimonial> TGetList()
+        {
+            return _testimonalDal.GetList();
+        }
+
+        public void TUpdate(Testimonial t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Traversal/ViewComponents/Default/_Testimonial.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.ViewComponents.Default
+{
+    public class _Testimonial : ViewComponent
+    {
+        TestimonalManager testimonalManager = new TestimonalManager(new EfTestimonialDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = testimonalManager.TGetList();
+            return View(values);
+        }
+    }
+}
